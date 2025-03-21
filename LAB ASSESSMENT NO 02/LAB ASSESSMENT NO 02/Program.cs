f09ec1f8c0401bf916bf051c2b2d02aacc5e9d03
Saad-Khalid-Coder <msaadkhalid1192@gmail.com>
2025-03-21 10:27:25 +0500

    Add files via upload

---
+++ LAB ASSESSMENT NO 02/LAB ASSESSMENT NO 02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_ASSESSMENT_NO_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t***************LAB ASSESSMENT NO 02***************");
+            Console.WriteLine("NAME:");
+            Console.WriteLine("\tMUHAMMAD SAAD KHALID");
+            Console.WriteLine("ROLL NO:");
+            Console.WriteLine("\t\t24_NTU_CS_FL_1192");
+            Console.WriteLine("SUBMITTED TO:");
+            Console.WriteLine("\t\t\tMAM TAHREEM JALIL");
+        }
+    }
+}
